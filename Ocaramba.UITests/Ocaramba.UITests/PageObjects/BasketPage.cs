f95dc4afec028c215645a4c4ce8d3fc3282c7ddd
Pawel K <pawel.kreski@yahoo.com>
2020-03-30 16:31:33 +0200

    first test case (#2)

---
+++ Ocaramba.UITests/Ocaramba.UITests/PageObjects/BasketPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+using NUnit.Framework;
+using Ocaramba;
+using Ocaramba.Extensions;
+using Ocaramba.Types;
+
+namespace Allegro.AcceptanceTests.PageObjects
+{
+    public class BasketPage : ProjectPageBase
+    {
+        private static readonly NLog.Logger Logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+        /// <summary>
+        /// Locators for elements
+        /// </summary>
+        private readonly ElementLocator firstBasketItem = new ElementLocator(Locator.CssSelector, "section seller-offers div div div div div div div div a");
+
+        public BasketPage(DriverContext driverContext)
+            : base(driverContext)
+        {
+        }
+
+        public string GetFirstItemName()
+        {
+            Logger.Info(CultureInfo.CurrentCulture, "Getting first item name");
+            return this.Driver.GetElements(firstBasketItem, 1)[0].Text;
+        }
+
+    }
+}
---
+++ Ocaramba.UITests/Ocaramba.UITests/Tests/BasketTests.cs
@@ -0,0 +1,36 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/ObjectivityLtd/Ocaramba 
+
+using System.Globalization;
+using Allegro.AcceptanceTests.PageObjects;
+using NUnit.Framework;
+
+namespace Allegro.AcceptanceTests.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Fixtures)]
+    public class BasketTests : ProjectTestBase
+    {
+        [Test, Description("Test that add item to cart and checks that the item is indeed in a cart")]
+        public void AddToBasketTest()
+        {
+
+            var homePage = new MainPage(this.DriverContext);
+            homePage.OpenHomePage();
+            homePage.AcceptDataConsent();
+            var resultsPage = homePage.SearchForProduct("rower");
+            resultsPage.ClickOnSecondHandCbx();
+            resultsPage.EnterPriceFrom(201);
+            var itemPage = resultsPage.ClickItem(0);
+            
+            var expected = itemPage.GetItemTitle();
+            
+            itemPage.ClickAddToCart();
+            var basketPage = itemPage.ClickGoToCart();
+
+            var actual = basketPage.GetFirstItemName();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
