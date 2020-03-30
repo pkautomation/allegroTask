1d7f7f72e0c1353d3b7007492f4bbea978b8799d
Pawel Kreski <pawel.kreski@falck.com>
2020-03-30 13:27:22 +0200

    initial work - project creation

---
+++ Ocaramba.UITests/Ocaramba.UITests/ProjectPageBase.cs
@@ -0,0 +1,18 @@
+﻿using Ocaramba;
+using OpenQA.Selenium;
+
+namespace Allegro.AcceptanceTests
+{
+    public partial class ProjectPageBase
+    {
+        public ProjectPageBase(DriverContext driverContext)
+        {
+            this.DriverContext = driverContext;
+            this.Driver = driverContext.Driver;
+        }
+
+        protected IWebDriver Driver { get; set; }
+
+        protected DriverContext DriverContext { get; set; }
+    }
+}
